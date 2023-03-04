e2813a258e183ef54fd9d23bba779ff5bd46de4e
MacBook <diagnec341@gmail.com>
2023-03-04 13:05:49 +0000

    Fix build

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,6 @@ const config = {
   overrides: [
     {
       extends: [
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
       ],
       files: ["*.ts", "*.tsx"],
       parserOptions: {
