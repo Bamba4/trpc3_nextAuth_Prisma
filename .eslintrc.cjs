c7cf71ad04a12d0199fbe388d2b9d3c089c58e87
MacBook <diagnec341@gmail.com>
2023-03-04 13:13:48 +0000

    Fix bug

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,7 @@ const config = {
   overrides: [
     {
       extends: [
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
       ],
       files: ["*.ts", "*.tsx"],
       parserOptions: {
--- src/server/api/routers/topic.ts
+++ src/server/api/routers/topic.ts
@@ -11,7 +11,7 @@ export const topicRouter = createTRPCRouter({
     getAll: publicProcedure.query(({ ctx }) => {
         return ctx.prisma.topic.findMany();
     }),
-    createTopic: protectedProcedure.input(z.object({ title: z.string() })).mutation(({ ctx, input }: {ctx: any, input: any}) => {
+    createTopic: protectedProcedure.input(z.object({ title: z.string() })).mutation(({ ctx, input }) => {
         return ctx.prisma.topic.create({
             data: {
                 title: input.title,
